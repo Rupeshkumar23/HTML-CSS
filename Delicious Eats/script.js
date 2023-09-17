cec268cb8ec94a6ff4c853132c454a0ca19e6f2b
Rupeshkumar23 <87306370+Rupeshkumar23@users.noreply.github.com>
2023-09-17 15:59:41 +0530

    Delete Delicious Eats directory

--- Delicious Eats/script.js
+++
@@ -1,9 +0,0 @@
-const menuIcon = document.querySelector("#menu_icon");
-const navbar = document.querySelector(".navbar");
-const navbg = document.querySelector(".nav-bg");
-
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-  navbg.classList.toggle("active");
-});
